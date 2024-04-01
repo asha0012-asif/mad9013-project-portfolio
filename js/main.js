bd679f67c033ae0124a2c17dc6ae71e49b1cb1d3
Asif Ashadullah <156141923+asha0012-asif@users.noreply.github.com>
2024-03-31 23:45:26 -0400

    Complete basic header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const APP = {
+    headerNavToggle: document.getElementById("header__nav-toggle"),
+
     init: () => {
         console.log("initialized website");
+
+        APP.headerNavToggle.addEventListener("click", APP.toggleNav);
+    },
+
+    toggleNav: (ev) => {
+        ev.preventDefault();
+
+        let headerNav = document.querySelector(".header__nav");
+        headerNav.classList.toggle("open");
+        
+        let headerNavToggle = APP.headerNavToggle;
+        headerNavToggle.classList.toggle("open");
     }
 }
 
